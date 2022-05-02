d7bb811a5db72731ff9df74a3aa67be3bb311748
Chioma2000 <jozinnachioma@gmail.com>
2022-05-02 13:51:02 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
-#include "main.h"
-#include <unistd.h>
-#include <stdlib.h>
-/**
- * main - Copies the content of a file to another file.
- * @ac: Argument count
- * @av: argument values
- * Return: 0 on succes, -1 on error.
- */
-int main(int ac, char *av[])
-{
-return (1);
-return (0);
-}
+ #include <stdio.h> 
+    #include <elf.h> 
+
+    int main(void) 
+    { 
+    	FILE *fd = fopen("./cp", "rb"); 
+    #if defined (__linux) 
+    #  if defined(__x86_64) 
+    	Elf64_Ehdr Elf_Ehdr; 
+    #  elif defined(__i386) 
+    	Elf32_Ehdr Elf_Ehdr; 
+    #  endif	 
+    #endif 
+    	fread(&Elf_Ehdr, sizeof(char), sizeof(Elf_Ehdr), fd); 
+     
+    	int i; 
+    	unsigned char ident[4], magic[4]; 
+    	for (i = 0; i < 4; i++) { 
+    		ident[i] = Elf_Ehdr.e_ident[i]; 
+    	} 
+    	fclose(fd); 
+     
+    	snprintf(magic, sizeof(magic), "%s", &ident[sizeof(ident)+1]); 
+    	printf("%s\n", magic); 
+     
+    	return 0; 
+    } 
